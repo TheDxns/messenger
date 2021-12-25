7a9fcb68ad8ab46ef5899f9613265b99a6650856
TheDxns <denis.lukasczyk@gmail.com>
2021-12-25 17:49:53 +0100

    initialized user service

---
+++ src/main/java/io/github/thedxns/messenger/logic/UserService.java
@@ -0,0 +1,22 @@
+package io.github.thedxns.messenger.logic;
+
+import io.github.thedxns.messenger.model.User;
+import io.github.thedxns.messenger.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /*List<User> getAllContacts(String username) {
+        return userRepository.getContactsByUser(username);
+    }*/
+}
